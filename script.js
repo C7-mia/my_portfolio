2d2f8c190f1149a5c132b4084476c4deec3be35b
C7-mia <akidirita4@gmail.com>
2026-04-13 02:48:47 +0300

    Add smooth scroll and contact form handler

---
+++ script.js
@@ -0,0 +1,11 @@
+function scrollToSection(id) {
+    document.getElementById(id).scrollIntoView({
+        behavior: "smooth"
+    });
+}
+
+// Contact form handler
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Message sent successfully!");
+});
